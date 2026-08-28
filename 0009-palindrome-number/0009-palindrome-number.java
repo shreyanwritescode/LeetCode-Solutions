578970b480a6efbf6dae8f1db7a20260f985f52e
shreyan <smffpu2007@gmail.com>
2026-08-29 01:44:08 +0530

    Time: 4 ms (100%), Space: 45.8 MB (87.03%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int num=x;
+        int rev=0,rem=0;
+        while(num>0){
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        if(x==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
